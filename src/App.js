0be3fa9f6161c448c447f39d3696abea6df61796
vibecode <rumashkin@gmail.com>
2019-10-03 00:15:43 +0200

    add solution

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/*
+Create a `withStorage` higher order component that manages saving and retrieving
+the `sidebarIsOpen` state to local storage
+*/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './index.css'
+import React from 'react'
+import { MdMenu } from 'react-icons/md'
+import { set, get, subscribe } from './local-storage'
+
+const withStorage = (storageKey, default_) => Component => {
+  return class extends React.Component {
+    //allows to unit test wrapped component
+    static WrappedComponent = Component
+
+    state = {
+      [storageKey]: get(storageKey, default_)
+    }
+
+    componentDidMount() {
+      this.unsubscribe = subscribe(() => {
+        this.setState({
+          [storageKey]: get(storageKey)
+        })
+      })
+    }
+
+    componentWillUnmount() {
+      this.unsubscribe()
+    }
+
+    render() {
+      return (
+        <Component
+          {...this.props}
+          {...this.state}
+          setStorage={value => set(storageKey, value)}
+        />
+      )
+    }
+  }
 }
 
-export default App;
+class App extends React.Component {
+  render() {
+    const { sidebarIsOpen, setStorage } = this.props
+    return (
+      <div className="app">
+        <header>
+          <button
+            className="sidebar-toggle"
+            title="Toggle menu"
+            onClick={() => {
+              setStorage(!sidebarIsOpen)
+            }}
+          >
+            <MdMenu />
+          </button>
+        </header>
+
+        <div className="container">
+          <aside className={sidebarIsOpen ? 'open' : 'closed'} />
+          <main />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default withStorage('sidebarIsOpen', true)(App)
